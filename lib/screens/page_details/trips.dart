efd6ef2de17dac3286ccecff1d97e73f0340bcec
James Min <Jamesmin90@gmail.com>
2020-03-19 22:43:06 +0100

    new folders and files

---
+++ lib/screens/page_details/trips.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Trips extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Trips')
+      ),
+      body: Text('Hello')
+    );
+  }
+}
