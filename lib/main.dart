dbb1927feec18b8da11bdf82e8bc78bd9ca0a93a
James Min <Jamesmin90@gmail.com>
2020-03-19 23:49:26 +0100

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,30 @@
 import 'package:flutter/material.dart';
-import 'csm.dart';
+import 'screens/page_details/home.dart';
+import 'package:csm/screens/page_details/events.dart';
+import 'package:csm/screens/page_details/trips.dart';
+import 'package:csm/screens/page_details/about.dart';
+
+
 void main() => runApp(CSM());
 
+class CSM extends StatelessWidget {
+ @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF263238),
+      ),
+      title: "CSM!",
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+        '/events': (context) => Events(),
+        '/trips': (context) => Trips(),
+        '/about': (context) => About(),
+      }
+       
+      ); 
+    
+  } 
+  
+  }
--- lib/screens/page_details/home.dart
+++ lib/screens/page_details/home.dart
@@ -1,42 +1,42 @@
-import 'home_banner.dart';
+import 'package:csm/screens/components/home_banner.dart';
 import 'package:flutter/material.dart';
 
-class HomneWidget extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Expanded buildlink({String imageName, String page}) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10.0)),
+        child: FlatButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '$page');
+          },
+          child: Image.asset(
+            "assets/images/$imageName",
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Center(
-        child: new ListView(
-          children: [
-            ImageBanner("assets/images/Eisen.jpg"),
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: Image.asset(
-                "assets/images/Eisen.jpg",
-                fit: BoxFit.cover,
-                height: 150,
-                width: 150,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: Image.asset(
-                "assets/images/Eisen.jpg",
-                fit: BoxFit.cover,
-                height: 150,
-                width: 150,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: Image.asset(
-                "assets/images/Eisen.jpg",
-                fit: BoxFit.cover,
-                height: 150,
-                width: 150,
-              ),
-            ),
-          ]
-        )
-      );
-    }
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Home"),
+      ),
+      body: new Center(
+          child: Column(children: <Widget>[
+        ImageBanner("assets/images/Eisen.jpg"),
+        buildlink(imageName: "Paris2.jpg", page: '/trips'),
+        buildlink(imageName: "Thursday.jpg", page: '/events'),
+        buildlink(imageName: "NCT.jpg", page: '/about')
+      ])),
+    );
+  }
 }
