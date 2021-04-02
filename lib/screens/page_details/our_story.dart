b81a93708433a9eda6284de2f9fd9e2f8ec6adb7
James <Jamesmin90@gmail.com>
2021-04-02 18:27:25 +0200

    fixed about us page

--- lib/screens/page_details/our_story.dart
+++ lib/screens/page_details/our_story.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:csm/screens/components/getdata.dart';
@@ -7,33 +8,30 @@ class Ourstory extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ColorfulSafeArea(
-        color: Color(0xFF263238),
+        color: Color.fromRGBO(0, 110, 160, 1),
         child: CustomScrollView(
           slivers: [
             SliverAppBar(
               iconTheme: IconThemeData(color: Color.fromRGBO(0, 110, 160, 1)),
+              expandedHeight: 60.0,
               backgroundColor: Colors.white,
-              expandedHeight: 140.0,
               floating: true,
               pinned: true,
-              //snap: false,
               flexibleSpace: FlexibleSpaceBar(
                   title: SizedBox(
-                height: 100,
+                height: 120,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
-                  //crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
-                    SizedBox(height: 10),
                     Container(
-                      padding: EdgeInsetsDirectional.fromSTEB(24, 0, 0, 0),
+                      //padding: EdgeInsetsDirectional.fromSTEB(24, 0, 0, 0),
                       width: double.infinity,
                       child: Text(
                         'Unsere Geschichte',
                         style: TextStyle(
                           fontWeight: FontWeight.w900,
                           color: Color.fromRGBO(0, 110, 160, 1),
-                          fontSize: 25,
+                          fontSize: 18,
                         ),
                         textAlign: TextAlign.left,
                       ),
@@ -59,8 +57,8 @@ class Ourstory extends StatelessWidget {
                             crossAxisAlignment: CrossAxisAlignment.stretch,
                             children: [
                               Container(
-                                child:
-                                    Image.network(snapshot.data[index].image),
+                                child: CachedNetworkImage(
+                                    imageUrl: snapshot.data[index].image),
                               ),
                               SizedBox(
                                 height: 30.0,
